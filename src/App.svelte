8d4e1451906b706dc7f65f48b8bc735fc3397028
graceXYZ <grace@inventxyz.com>
2023-05-01 13:52:52 -0400

    ok

---
+++ src/App.svelte
@@ -0,0 +1,25 @@
+<script>
+  import svelteLogo from './assets/svelte.svg'
+  import viteLogo from '/vite.svg'
+  import Counter from './lib/Counter.svelte'
+</script>
+
+<main>
+
+  <h1>InventXYZ Code Snips</h1>
+
+  <p>
+    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
+  </p>
+
+  <p class="read-the-docs">
+    Click on a section to start learning!
+  </p>
+</main>
+
+<style>
+  
+  .read-the-docs {
+    color: #888;
+  }
+</style>
