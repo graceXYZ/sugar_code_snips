6d7e575ded728cc4650ac68f786e3958c55faf48
graceXYZ <grace@inventxyz.com>
2023-05-01 13:53:53 -0400

    ok

--- src/App.svelte
+++ src/App.svelte
@@ -6,7 +6,7 @@
 
 <main>
 
-  <h1>InventXYZ Code Snips</h1>
+  <h1>InventXYZ Code Snips!</h1>
 
   <p>
     Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
